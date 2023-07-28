15f2bc943ed241a197fc2fe1a9c746556ca4b227
Ouroboros420 <42john42smith@googlemail.com>
2023-07-28 13:16:42 +0200

    pantah: #BringTheShishuBack

--- bootleg_panther.mk
+++ bootleg_panther.mk
@@ -4,9 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common Bootleg stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
@@ -17,7 +17,7 @@ include device/google/pantah/panther/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7
-PRODUCT_NAME := lineage_panther
+PRODUCT_NAME := bootleg_panther
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -30,3 +30,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := google/panther/panther:14/UP1A.231005.007/10754064:user/release-keys
 
 $(call inherit-product, vendor/google/panther/panther-vendor.mk)
+
+## More Bootleg stuff
+# Maintainer Prop
+DEVICE_MAINTAINERS := JochenSplifficus
+BOOTLEGGERS_BUILD_TYPE := Shishufied
+TARGET_BOOTLEG_ARCH := arm64
